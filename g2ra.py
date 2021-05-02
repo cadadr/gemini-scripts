c1937413f021e2c27bad225e188fc76d747ab4c0
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-05-02 16:26:18 +0300

    g2ra.py: rss feeds and a bit of customisation

--- g2ra.py
+++ g2ra.py
@@ -4,7 +4,7 @@
 # requirements: flask, ignition-gemini
 
 # TODO
-# - implement rss
+# - XXX validate ttl and author (and other stuff more stricter)
 # - deal with redirects
 # - report errors better
 #   - try couple redirects
@@ -12,17 +12,19 @@
 
 import ignition
 import io
-import pprint
 import sys
 import xml.etree.ElementTree as ET
 
-from datetime import datetime
+from datetime import datetime, timezone
 from flask import Flask, request, url_for
 from urllib.parse import urljoin, urlparse, urlunparse
 from wsgiref.handlers import CGIHandler
 from xml.dom import minidom
 
-pp = pprint.PrettyPrinter(indent=4)
+
+TTL_DEFAULT = "1800"
+AUTHOR_DEFAULT = "Unknown Author Jr."
+
 
 def parse(string):
     lines = string.split("\n")
@@ -88,7 +90,8 @@ def expect_feed_item(line):
     try:
         _, link, description = line.split(maxsplit=2)
         date_string_maybe, rest = description.split(maxsplit=1)
-        date = datetime.strptime(date_string_maybe, "%Y-%m-%d")
+        date = datetime.strptime(date_string_maybe,
+                "%Y-%m-%d").astimezone(tz=timezone.utc)
         return {
             'link': link,
             'description': rest,
@@ -100,23 +103,35 @@ def expect_feed_item(line):
         return None
 
 
-def convert(url, type_, content):
+def convert(url, type_, content, **kwargs):
     data = parse(content)
+    data.update(kwargs)
     if type_ == 'atom':
         return atom_feed(url, data)
     if type_ == 'rss':
         return rss_feed(url, data)
+
+def xml_stringify(element):
+    # This is disgusting but it works and I already sank too much time
+    # into converting fucking XML to a bloody string so fuck it.
+    xmlstr = ET.tostring(element, encoding="unicode")
+    pretty = minidom.parseString(xmlstr).toprettyxml(encoding="UTF-8")
+    return pretty
 
 
 def atom_feed(url, data):
     feed = ET.Element('feed', xmlns='http://www.w3.org/2005/Atom')
     title = ET.SubElement(feed, "title")
     title.text = data['title']
-    link = ET.SubElement(feed, "link", href=url)
+    link = ET.SubElement(feed, "link", href=url, rel="self")
     updated = ET.SubElement(feed, "updated")
     updated.text = data['updated'].isoformat()
     id_ = ET.SubElement(feed, "id")
     id_.text = url
+    subtitle_maybe = data.get('subtitle')
+    if subtitle_maybe is not None:
+        subtitle = ET.SubElement(feed, 'subtitle')
+        subtitle.text = subtitle_maybe
 
     for link in data['links']:
         entry = ET.SubElement(feed, 'entry')
@@ -134,21 +149,73 @@ def atom_feed(url, data):
             href = href._replace(path=path)
             href = href._replace(scheme='gemini')
         href = urlunparse(href)
-        link_ = ET.SubElement( entry, "link", href=href, rel="alternate")
+        link_ = ET.SubElement(entry, "link", href=href, rel="alternate")
         entry_id = ET.SubElement(entry, "id")
         entry_id.text = href
+        author = ET.SubElement(entry, "author")
+        author_name = ET.SubElement(author, "name")
+        author_name.text = data['author']
 
-    # tree = ET.ElementTree(feed)
-
-    # This is disgusting but it works and I already sank too much time
-    # into converting fucking XML to a bloody string so fuck it.
-    xmlstr = ET.tostring(feed, encoding="unicode")
-    pretty = minidom.parseString(xmlstr).toprettyxml(encoding="UTF-8")
-    return pretty
+    return xml_stringify(feed)
 
 
+# Adapted from http://johnbokma.com/blog/2019/10/09/rfc-822-and-rfc-3339-dates-in-python.html
+def rss_date(date_obj):
+    # XXX This is weird but I will think about it later.
+    end_of_day = datetime.strptime(
+            f'{date_obj.strftime("%F")} 00:00:00',
+            '%Y-%m-%d %H:%M:%S').astimezone()
+    ctime = date_obj.ctime()
+    return (f'{ctime[0:3]}, {end_of_day.day:02d} {ctime[4:7]}'
+                + end_of_day.strftime(' %Y %H:%M:%S %z'))
+
+
+# Adapted copy of `atom_feed'.
 def rss_feed(url, data):
-    ...
+    feed = ET.Element('rss', version='2.0', attrib={
+        "xmlns:atom": "http://www.w3.org/2005/Atom"})
+    channel = ET.SubElement(feed, "channel")
+    title = ET.SubElement(channel, "title")
+    title.text = data['title']
+    link = ET.SubElement(channel, "link")
+    link.text = url
+    atom_link = ET.SubElement(channel,
+            "atom:link", rel="self", href=url)
+    pub_date = rss_date(data['updated'])
+    pubDate = ET.SubElement(channel, "pubDate")
+    pubDate.text = pub_date
+    lastBuildDate = ET.SubElement(channel, "lastBuildDate")
+    lastBuildDate.text = pub_date
+    param_ttl = data.get(ttl)
+    ttl = ET.SubElement(channel, param_ttl)
+    ttl.text = '1800' # TODO configable
+    description_maybe = data.get('subtitle')
+    if description_maybe is not None:
+        description = ET.SubElement(channel, 'description')
+        description.text = description_maybe
+
+    for link in data['links']:
+        entry = ET.SubElement(channel, 'item')
+        title = ET.SubElement(entry, "title")
+        title.text = link['description']
+        pubDate = ET.SubElement(entry, "pubDate")
+        pubDate.text = rss_date(link['date'])
+        href = urlparse(link['link'])
+        url_ = urlparse(url)
+        if href.netloc == '':
+            href = href._replace(netloc=url_.netloc)
+            href = href._replace(netloc=url_.netloc)
+            path = urljoin(url_.path, href.path)
+            href = href._replace(path=path)
+            href = href._replace(scheme='gemini')
+        href = urlunparse(href)
+        link_ = ET.SubElement( entry, "link")
+        link_.text = href
+        # TODO: allow UUID for GUIDs?
+        guid = ET.SubElement(entry, "guid", isPermaLink="true")
+        guid.text = href
+
+    return xml_stringify(feed)
 
 
 app = Flask(__name__)
@@ -161,12 +228,15 @@ def main():
     gem_url = ignition.url(param_url)
     if param_type not in ['atom', 'rss']:
         raise Exception(f"unknown feed type: {param_type}")
+    param_ttl = request.args.get("ttl", TTL_DEFAULT)
+    param_author = request.args.get("author", AUTHOR_DEFAULT)
     response = ignition.request(str(gem_url), timeout=5.0)
     if isinstance(response, ignition.InputResponse):
         raise Exception("input")
     elif isinstance(response, ignition.SuccessResponse):
         # todo: accepts
-        xml = convert(gem_url, param_type, str(response))
+        xml = convert(gem_url, param_type, str(response),
+                        ttl=param_ttl, author=param_author)
         return xml
     elif isinstance(response, ignition.RedirectResponse):
         # todo: try once
